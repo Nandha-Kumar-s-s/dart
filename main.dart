bd6dea74934ea200fb61c13ea73ff55289800f9e
Nandha-Kumar-s-s <nandhakumar834427@gmail.com>
2025-01-31 11:20:23 +0530

    1. Initial commit

---
+++ main.dart
@@ -0,0 +1,19 @@
+void main(){
+
+  // Dart variables
+  // <datatype> <variable_name> = <value>;
+
+  int x = 10;
+  double y = 20;
+  String name = "Nandhu";
+  bool isadult = true;
+  dynamic someValue = "Any value we can store using dynamic variable";
+
+  // Print statement
+  print(x.isEven);
+  print("y = $y");
+  print("x + y = ${x + y}");
+  print("Hello, ${name}");
+  print("Boolean value = ${isadult}");
+  print("Dynamic value = $someValue");
+}
